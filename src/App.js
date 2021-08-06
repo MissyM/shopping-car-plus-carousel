d52250871cfb5c0d4b1a1427b1da3d82ce3e7230
Milena <mcabrerapatino@gmail.com>
2021-08-05 22:20:53 -0500

    refactor carousel

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 
 import './App.scss'
 import Header from './components/Header'
-import Carousel from './components/Carousel'
+import Slider from './components/Slider'
 import Article from './components/Article'
 import * as api from './api'
 
@@ -18,20 +18,22 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Carousel>
-        {articles.map((article) => (
-          <Article
-            key={article.id}
-            image={article.image}
-            supplier={article.supplier}
-            netContent={article.net_content}
-            title={article.title}
-            priceReal={article.price_real}
-            unitsSf={article.units_sf}
-            sellos={article.sellos}
-          />
-        ))}
-      </Carousel>
+      <div className="container">
+        <Slider>
+          {articles.map((article) => (
+            <Article
+              key={article.id}
+              image={article.image}
+              supplier={article.supplier}
+              netContent={article.net_content}
+              title={article.title}
+              priceReal={article.price_real}
+              unitsSf={article.units_sf}
+              sellos={article.sellos}
+            />
+          ))}
+        </Slider>
+      </div>
     </div>
   )
 }
---
+++ src/components/Slider.jsx
@@ -0,0 +1,31 @@
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+import styles from './slider.module.scss'
+
+const Slider = ({ children }) => {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 3,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  }
+  return (
+    <Carousel
+      containerClass={styles.container}
+      partialVisible
+      responsive={responsive}
+    >
+      {children}
+    </Carousel>
+  )
+}
+
+export default Slider
